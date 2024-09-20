90b4ae85eeaffe7227cc54d32bdda3af85978879
Elnur <63106963+KomoGit@users.noreply.github.com>
2024-09-20 20:42:03 +0400

    New entity.

---
+++ src/Domain/Entities/JobEntity.cs
@@ -0,0 +1,33 @@
+﻿using SharedKernel.Domain.Seedwork;
+
+namespace Domain.Entities
+{
+    public class JobEntity : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public int Views { get; private set; }
+        public DateTime ExpirationDate { get; private set; }
+        public DateTime CreateDate { get; private set; }
+        public DateTime UpdateDate { get; private set; }
+
+
+        public void SetDetails(string name, string description, DateTime expDate)
+        {
+            ExpirationDate = expDate;
+            Name = name;
+            Description = description;
+            CreateDate = DateTime.UtcNow;
+        }
+
+        public void IncreaseViews()
+        {
+            Views++;
+        }
+
+        public void UpdateDateTime()
+        {
+            UpdateDate = DateTime.UtcNow;   
+        }
+    }
+}
