1087ca183d39ad549e693d429a4a213cd37063db
Elnur <63106963+KomoGit@users.noreply.github.com>
2024-09-20 20:40:08 +0400

    Generic repo pattern

---
+++ src/Application/Usecase/Repository.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using Domain.Exceptions;
+using Infrastructure.DAL;
+using SharedKernel.Domain.Seedwork;
+using System.Data;
+
+namespace Application.Usecase
+{
+    public class Repository<T>(AppDbContext context) : IRepository<T> where T : BaseEntity
+    {
+        private readonly IDbConnection _connection = context.CreateConnection();
+
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            string query = $"SELECT * FROM {typeof(T).Name.ToLower()}s";
+            IEnumerable<T> data = await _connection.QueryAsync<T>(query);
+            return data.ToList();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            string query = $"SELECT * FROM {typeof(T).Name.ToLower()}s WHERE Id = @Id";
+            return await _connection.QueryFirstOrDefaultAsync<T>(query, new { Id = id }) 
+                ?? throw new EntityNotFoundException<T>();
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SharedKernel.Domain/Seedwork/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace SharedKernel.Domain.Seedwork
+{
+    public interface IRepository<T>
+    {
+        Task<T> AddAsync(T entity);
+        Task<List<T>> GetAllAsync();
+        Task<T> GetById(int id);
+        T Update(T entity);
+        bool Delete(T entity);
+    }
+}
