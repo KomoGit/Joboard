3a251866e7bf73d5442af85d6f3cfcdb48fd02fb
Elnur <63106963+KomoGit@users.noreply.github.com>
2024-09-21 21:17:49 +0400

    Schema prop

--- src/SharedKernel.Domain/Settings/DatabaseSettings.cs
+++ src/SharedKernel.Domain/Settings/DatabaseSettings.cs
@@ -4,6 +4,7 @@
     {
         public string? Server { get; set; }
         public string? Database { get; set; }
+        public string? Schema { get; set; }
         public string? UserId { get; set; }
         public string? Password { get; set; }
     }
